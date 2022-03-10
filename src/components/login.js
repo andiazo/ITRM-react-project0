a9a172361e78bbbf58c089f1d3c2cf645be1192e
Andres Diaz <andiazo@unal.edu.co>
2022-03-10 17:21:54 -0500

    Add state and event

--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,22 @@
 import React from 'react';
 
 class Login extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLogged: true,
+            };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(prevState => ({
+            isLogged: !prevState.isLogged
+        }))
+        window.open("https://metamask.io/");
+    }
+
     render(){
         return(
             <div className='login'>
@@ -8,9 +24,9 @@ class Login extends React.Component {
                     <div className='container'>
                         <h1>Hello friend!</h1>
                         <p>Welcome back. Login to view amazing clothes</p>
-                        <button>
+                        <button onClick={this.handleClick}>
                                 <img src={`${process.env.PUBLIC_URL}/metamask-white.png`}></img>
-                                <span>Login with metamask</span>
+                                <span>{this.state.isLogged ? 'Logged with metamask':'If you don\'t have metamask, please install it' }</span>
                         </button>
                     </div>
                 </div>
