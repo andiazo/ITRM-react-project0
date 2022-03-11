0cdda5fcde2575cdef207abd266b9725e56a7cf0
Andres Diaz <andiazo@unal.edu.co>
2022-03-10 19:53:56 -0500

    Add promise

--- src/components/login.js
+++ src/components/login.js
@@ -6,9 +6,34 @@ class Login extends React.Component {
         super(props);
         this.state = {
             isLogged: true,
+            promise: '',
             };
         this.handleClick = this.handleClick.bind(this);
+        this.handlePromise = this.handlePromise.bind(this);
     }
+
+    handlePromise(){
+        let newPromise = new Promise((resolve, reject) => {
+            resolve('Promesa del login resuelta');
+        })    
+    
+        newPromise.then( result => {
+            console.log(result)
+            this.setState({
+                promise: result
+            })
+            
+         }, function(error) {
+            console.log(error)
+            this.setState({
+                promise: error
+            })
+            
+         });
+         
+    }
+    
+
 
     handleClick() {
         this.setState(prevState => ({
@@ -24,6 +49,7 @@ class Login extends React.Component {
                     <div className='container'>
                         <h1>Hello friend!</h1>
                         <p>Welcome back. Login to view amazing clothes</p>
+                        <p onClick={this.handlePromise}>Promesa: {this.state.promise}</p>
                         <button onClick={this.handleClick}>
                                 <img src={`${process.env.PUBLIC_URL}/metamask-white.png`}></img>
                                 <span>{this.state.isLogged ? 'Logged with metamask':'If you don\'t have metamask, please install it' }</span>
