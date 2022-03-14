5fbb2df9d35e7345034c53f3d454b92a030b62e9
Andrés Díaz <36974713+andiazo@users.noreply.github.com>
2022-03-14 14:46:10 -0500

    Add Login View

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Navbar from '../components/navbar';
+import Login from '../components/login';
+import Main from '../components/main';
+import Footer from '../components/footer';
 
 
 class LoginView extends React.Component {
     render() {
         return(
-            
+            <>
+                <Navbar />
+                <Main>
+                    <Login />
+                </Main>
+                <Footer />
+            </>
         )
     }
 }
