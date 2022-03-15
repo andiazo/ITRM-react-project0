54eaf4f4317bdc4e18f0aa01c1d80dd04b1c5d76
andiazo <andiazo@unal.edu.co>
2022-03-15 11:20:52 -0500

    Add routing

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,23 @@
 import React from 'react';
-import BrowserRouter from 'react-router-dom';
+import BrowserRouter, { Routes, Route } from 'react-router-dom';
+import SellerView from './views/SellerView';
+import BuyerView from './views/BuyerView';
+import LoginView from './views/LoginView';
+import ProductView from './views/ProductView';
 
 
 class Router extends React.Component {
     // TODO
+    render() {
+        return(
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/login" element={<LoginView />} />
+                    <Route path="/buy" element={<BuyerView />} />
+                    <Route path="/sell" element={<SellerView />} />
+                    <Route path="/products" element={<ProductView />} />
+                </Routes>
+            </BrowserRouter>
+        )
+    }
 }
